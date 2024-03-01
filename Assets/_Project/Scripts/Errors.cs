b38850826103a0f465898642d6c9fc9b9aa26832
Basilísco <senstur.art@gmail.com>
2024-03-01 01:18:40 -0300

    3.7

--- Assets/_Project/Scripts/Errors.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Mistix{
-    public static class Errors{
-        public static void InstanceError(MonoBehaviour sender){
-            Debug.Log($"There's more than one Instance of {sender.name}");
-        }
-    }
-}
